0d84e9922a09e97579869cf486bc6f9b943d2d62
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-11-30 17:26:06 +0800

    guard reorgnize

---
+++ hidden/Assets/General/IEnumerableExtension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public static class IEnumerableExtension {
+    public static IEnumerable<T> InfinityRepeat<T>(this IEnumerable<T> list) {
+        for (;;)
+            foreach (var t in list)
+                yield return t;
+    }
+}
---
+++ hidden/Assets/Guard/guardMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class guardMove : MonoBehaviour
+{
+
+    public Transform[] CheckPoints;
+    private NavMeshAgent agent;
+    IEnumerator<Transform> nextTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        nextTarget = CheckPoints.InfinityRepeat().GetEnumerator();
+        nextTarget.MoveNext();
+        agent.destination = nextTarget.Current.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, nextTarget.Current.position) > 1) return;
+        nextTarget.MoveNext();
+        agent.destination = nextTarget.Current.position;
+    }
+}
