922d4a4da85e9791d00096f68ff9fdd3b73e4d20
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-11-30 12:36:39 +0800

    Practice Nav Mesh

---
+++ hidden/Assets/SetAsTarget.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAsTarget : MonoBehaviour
+{
+
+    // Use this for initialization
+    void OnMouseDown()
+    {
+        FindObjectOfType<NavMeshAgent>().destination = this.transform.position;
+    }
+}
---
+++ hidden/Assets/blink.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blink : MonoBehaviour
+{
+
+    private new Renderer renderer;
+
+    void Start()
+    {
+        renderer = this.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var c = renderer.material.color;
+        c.a = Mathf.Sin(Time.time);// / 3 + 0.5f;
+        renderer.material.color = c;
+    }
+}
