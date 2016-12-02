01c8c6f8a533077821ea22f348662a0a5ebe5910
Yu Hsuan Hung <deanwinchester0326@gmail.com>
2016-12-02 14:38:33 +0800

    teleport
    by idontwanttotell101

---
+++ hidden/Assets/teleport.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class teleport : MonoBehaviour
+{
+    public teleport tele;
+
+    List<GameObject> current = new List<GameObject>();
+
+    // Use this for initialization
+    void OnTriggerEnter(Collider c)
+    {
+        if (!(c.gameObject.tag == "Player")) return;
+        if (current.Exists(x => x == c.gameObject)) return;
+        tele.current.Add(c.gameObject);
+        float deltaY = c.gameObject.transform.position.y - transform.position.y;
+        c.gameObject.transform.position = tele.transform.position + Vector3.up * deltaY;
+        //c.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+
+
+    // Use this for initialization
+    void OnTriggerExit(Collider c)
+    {
+        current.Remove(c.gameObject);
+    }
+}
