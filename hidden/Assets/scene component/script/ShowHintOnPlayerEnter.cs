e755ea61616e44afd31682306513cf2ffceccc5f
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-12-03 18:36:15 +0800

    fence and demo scene

--- hidden/Assets/scene component/script/ShowHintOnPlayerEnter.cs
+++ hidden/Assets/scene component/script/ShowHintOnPlayerEnter.cs
@@ -7,6 +7,15 @@ public class ShowHintOnPlayerEnter : MonoBehaviour
     public FadeText hint;
     public string text;
     // Update is called once per frame
+
+    void Start()
+    {
+        if (hint == null)
+        {
+            hint = FindObjectOfType<FadeText>();
+        }
+    }
+
     void OnPlayerEnter(Collider c)
     {
         hint.Text = text;
---
+++ hidden/Assets/timer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Text>().text = "Time : " + Time.time.ToString();
+    }
+}
