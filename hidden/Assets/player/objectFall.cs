b65e58c2926c8d023f124626a484d90aa8fa6705
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-12-14 15:15:23 +0800

    script-fall
    debugging add rigidbody has return null

---
+++ hidden/Assets/player/objectFall.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class objectFall : MonoBehaviour
+{
+    private int countdown = 5;
+    public int ThresholdFrame = 5;
+    [Tooltip("Object To Fall.\nIf this field is null on Start(), the root object is used instead.")]
+    public GameObject target;
+
+    void Start()
+    {
+        countdown = ThresholdFrame;
+        if (target == null)
+            target = transform.root.gameObject;
+    }
+
+    void Update()
+    {
+        --countdown;
+        if (countdown < 0)
+        {
+            Fall();
+            this.enabled = false;
+        }
+    }
+
+    private void Fall()
+    {
+        var components = target.GetComponentsInChildren<Component>();
+        foreach (var com in components.Where(x => !(x is Renderer || x is Transform)))
+        {
+            GameObject.Destroy(com);
+        }
+        var rig = target.AddComponent<Rigidbody>();
+    }
+
+    void OnTriggerStay(Collider c)
+    {
+        countdown = ThresholdFrame;
+    }
+}
