348c3f22fcef781b8b0c20f8d83c26d3f212590f
Idontwanttotell101 <t105598018@ntut.org.tw>
2016-12-14 16:05:14 +0800

    Change Fall To Generic Button

--- hidden/Assets/player/objectFall.cs
+++ hidden/Assets/player/objectFall.cs
@@ -28,17 +28,7 @@ public class objectFall : MonoBehaviour
 
     private void Fall()
     {
-        var components = target.GetComponentsInChildren<Component>();
-        foreach (var com in components.Where(x => !(x is Renderer || x is Transform || x is objectFall)))
-        {
-            
-            if (com is Rigidbody) GameObject.DestroyImmediate(com);
-            if (com is Behaviour) (com as Behaviour).enabled = false;
-            else GameObject.Destroy(com);
-            
-            //GameObject.DestroyImmediate(com);
-        }
-        var rig = target.AddComponent<Rigidbody>();
+        target.AddComponent<FallOperator>();
     }
 
     void OnTriggerStay(Collider c)
